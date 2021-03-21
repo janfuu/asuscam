8645372420e91a6d8d3ae351ddecff2607260040
Jan Fuchsmann <jan.fuchsmann@gmail.com>
2021-03-21 16:26:41 +0100

    move ffmpeg to background

--- kamera.sh
+++ kamera.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Kopiert und dreht Video zur virtuellen Kamera
-ffmpeg -f v4l2 -i /dev/video9 -vf "vflip" -f v4l2 /dev/video0
+ffmpeg -f v4l2 -i /dev/video9 -vf "vflip" -f v4l2 /dev/video0 &
